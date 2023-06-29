85ff6c611495e22aaf1d2c97fae7b44025429f7e
Abdulaziz Fakhriddinov <mr_abdulaziz777@list.ru>
2023-06-29 19:05:10 +1000

    Update my_list.rb

--- my_list.rb
+++ my_list.rb
@@ -11,16 +11,13 @@ class MyList
     @list.each { |element| yield element }
   end
 end
-# Create our list
+
 list = MyList.new(1, 2, 3, 4)
 
-# Test #all?
 puts list.all? { |e| e < 5 } # true
 puts list.all? { |e| e > 5 } # false
 
-# Test #any?
 puts list.any? { |e| e == 2 } # true
 puts list.any? { |e| e == 5 } # false
 
-# Test #filter
 puts list.filter(&:even?) # [2, 4]
